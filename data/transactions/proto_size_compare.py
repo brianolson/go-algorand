7642675661126db910488c2d970a16d2a02410db
Brian Olson <brian.olson@gmail.com>
2021-04-20 23:03:15 +0000

    wip, more compare, can run against 2000000 mainnet txns

--- data/transactions/proto_size_compare.py
+++ data/transactions/proto_size_compare.py
@@ -5,6 +5,7 @@
 import json
 import logging
 import os
+import statistics
 import sys
 
 import algosdk
@@ -54,6 +55,30 @@ mappings = {
         'amt':'payment.amount',
         'rcv':'payment.receiver',
         'close':'payment.close_remainder_to',
+
+        # asset config
+        'apar':('acfg.asset_params', tpb.AssetParams),
+        'caid':'acfg.config_asset',
+
+        # asset transfer
+        'xaid':'axfr.xfer_asset',
+        'aamt':'axfr.asset_amount',
+        'asnd':'axfr.asset_sender',
+        'arcv':'axfr.asset_receiver',
+        'aclose':'axfr.asset_close_to',
+
+        # asset freeze
+        'fadd':'afrz.freeze_account',
+        'faid':'afrz.freeze_asset',
+        'afrz':'afrz.asset_frozen',
+
+        # keyreg
+        'votekey':'keyreg.vote_pk',
+        'selkey':'keyreg.selection_pk',
+        'votefst':'keyreg.vote_first',
+        'votelst':'keyreg.vote_last',
+        'votekd':'keyreg.vote_key_dilution',
+        'nonpart':'keyreg.nonparticipation',
     },
     tpb.MultisigSig:{
         'v':'version',
@@ -64,7 +89,40 @@ mappings = {
         'pk':'key',
         's':'signature',
     },
+    tpb.AssetParams:{
+        't':'total',
+        'dc':'decimals',
+        'df':'default_frozen',
+        'un':'unit_name',
+        'an':'asset_name',
+        'au':'url',
+        'am':'metadata_hash',
+        'm':'manager',
+        'r':'reserve',
+        'f':'freeze',
+        'c':'clawback',
+    },
+    tpb.LogicSig:{
+        'l':'logic',
+        'sig':'sig',
+        'msig':('msig', tpb.MultisigSig),
+        'arg':'args',
+    },
 }
+
+def pathget(ob, path):
+    try:
+        path = path.split('.')
+        if len(path) == 1:
+            return getattr(ob, path[0])
+        for elem in path:
+            ob = getattr(ob, elem)
+            if ob is None:
+                return ob
+        return ob
+    except:
+        logger.error('could not get %r from %r', path, ob)
+        raise
 
 def pathset(out, fm, v):
     path = fm.split('.')
@@ -94,13 +152,17 @@ def dict_to_pbob(d, out, clazz):
         # if v is None:
         #     continue
         fm = field_map[k]
-        if isinstance(fm, str):
-            pathset(out, fm, v)
-            continue
-        if isinstance(fm, tuple):
-            attrname, nclazz = fm
-            dict_to_pbob(v, getattr(out, attrname), nclazz)
-            continue
+        try:
+            if isinstance(fm, str):
+                pathset(out, fm, v)
+                continue
+            if isinstance(fm, tuple):
+                attrname, nclazz = fm
+                dict_to_pbob(v, pathget(out, attrname), nclazz)
+                continue
+        except:
+            logger.error('could not set out.%r from d[%r] (%r)', fm, k, v, exc_info=True)
+            raise
         raise Exception("don't know what to do for d[{}] -> {}.{}".format(k, clazz, fm))
 
 # input, block['block']['txns'][n] SignedTxnInBlk
@@ -119,8 +181,42 @@ def token_addr_from_args(args):
         addr = 'http://' + addr
     return token, addr
 
-def wat():
-    return
+class Compare:
+    def __init__(self, args):
+        self.args = args
+        self.txncount = 0
+        self.sizes = []
+        self.errcount = 0
+
+    def processBlock(self, block, block_num):
+        try:
+            self._processBlock(block, block_num)
+        except:
+            logger.error('error in block %d', block_num)
+            raise
+
+    def _processBlock(self, block, block_num):
+        intra = 0
+        for stxn in block['block'].get('txns',[]):
+            try:
+                self._processStxn(block_num, intra, stxn)
+            except Exception as e:
+                self.errcount += 1
+                if self.errcount > self.args.max_err:
+                    raise
+                logger.error('%d:%d bad txn %s', block_num, intra, e)
+            intra += 1
+            self.txncount += 1
+            if (self.args.txn_limit is not None) and (self.txncount >= self.args.txn_limit):
+                break
+    def _processStxn(self, block_num, intra, stxn):
+        mpbytes = msgpack.packb(stxn)
+        pbstxn = to_pb(stxn)
+        pbbytes = pbstxn.SerializeToString()
+        # TODO: reverse the encoding translation, pbbytes back to mppbytes to ensure that all the data is there and canonical msgpack can still be created
+        self.sizes.append((block_num, intra, len(mpbytes), len(pbbytes)))
+        #print('{}:{}\t{}\t{}'.format(block_num, intra, len(mpbytes), len(pbbytes)))
+
 
 def main():
     import argparse
@@ -130,6 +226,7 @@ def main():
     ap.add_argument('--algod-token', default=None, help='algod token')
     ap.add_argument('--txn-limit', default=None, type=int, help='stop after some number of txn')
     ap.add_argument('--block-start', default=0, type=int, help='block number to start at')
+    ap.add_argument('--max-err', default=0, type=int, help='maximum number of txn translation failures before quitting')
     ap.add_argument('--verbose', default=False, action='store_true')
     args = ap.parse_args()
 
@@ -142,8 +239,7 @@ def main():
     algod = AlgodClient(token, addr)
 
     block_num = args.block_start
-    txncount = 0
-    sizes = []
+    comp = Compare(args)
     while True:
         try:
             block = algod.block_info(block_num, response_format='msgpack')
@@ -152,30 +248,24 @@ def main():
         block = algosdk.encoding.msgpack.unpackb(block)
         #print(block['block'].keys())
         #print('r={} len(txns)={}'.format(block_num, len(block['block'].get('txns',[]))))
-        intra = 0
-        for stxn in block['block'].get('txns',[]):
-            #print('{}:{}'.format(block_num, intra))
-            mpbytes = msgpack.packb(stxn)
-            pbstxn = to_pb(stxn)
-            pbbytes = pbstxn.SerializeToString()
-            # TODO: reverse the encoding translation, pbbytes back to mppbytes to ensure that all the data is there and canonical msgpack can still be created
-            sizes.append((block_num, intra, len(mpbytes), len(pbbytes)))
-            print('{}:{}\t{}\t{}'.format(block_num, intra, len(mpbytes), len(pbbytes)))
-            intra += 1
-            txncount += 1
-            if (args.txn_limit is not None) and (txncount >= args.txn_limit):
-                break
+        comp.processBlock(block, block_num)
         block_num += 1
-        if (args.txn_limit is not None) and (txncount >= args.txn_limit):
+        if (args.txn_limit is not None) and (comp.txncount >= args.txn_limit):
             break
+    print('processed {} txns from block {} to {}'.format(comp.txncount, args.block_start, block_num))
     pbtot = 0
     mptot = 0
     count = 0
-    for block_num, intra, mplen, pblen in sizes:
+    ratios = []
+    for block_num, intra, mplen, pblen in comp.sizes:
         mptot += mplen
         pbtot += pblen
         count += 1
-    print('N={}, avg msgp {}, avg pb {}'.format(count, mptot/count, pbtot/count))
+        ratio = pblen / mplen
+        ratios.append(ratio)
+    rmean = statistics.mean(ratios)
+
+    print('N={}, avg msgp {}, avg pb {}; ratio min/avg/max {}/{}/{}'.format(count, mptot/count, pbtot/count, min(ratios), rmean, max(ratios)))
     return 0
 
 if __name__ == '__main__':
